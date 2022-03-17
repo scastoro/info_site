dc6120bcaacdffa4d1e44c2c01d4b3752ca18ca0
Sal Castoro <20549092+scastoro@users.noreply.github.com>
2022-03-17 09:58:34 -0400

    Use middleware to serve static files from /public
    Also tested out a custom logger middleware.

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public", "index.html"));
-});
+// Logger
+const myLogger = function (req, res, next) {
+  console.log("Request IP: " + req.ip);
+  console.log("Request Method: " + req.method);
+  console.log("Request date: " + new Date());
 
-app.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public", "contact-me.html"));
-});
+  next();
+};
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public", "about.html"));
-});
+app.use(myLogger);
+// Serve static pages from the public file
+app.use(express.static("public"));
 
+// Serve specific files for specific routes
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public", "index.html"));
+// });
+
+// app.get("/contact", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public", "contact-me.html"));
+// });
+
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public", "about.html"));
+// });
+
+// Serve 404.html for all routes that do not exists
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public", "404.html"));
 });
